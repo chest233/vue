9a79422903e5a28f5bd0e4029bb51e8783d7fd5c
chest <630601642@qq.com>
2021-12-20 18:02:46 +0800

    加入 webstorm 路径配置文件

---
+++ ws.config.js
@@ -0,0 +1,13 @@
+/*eslint-disable*/
+System.config({
+  "paths": {
+    "vue/*": './src/platforms/web/entry-runtime-with-compiler/*',
+    "compiler/*": './src/compiler/*',
+    "core/*": './src/core/*',
+    "shared/*": './src/shared/*',
+    "web/*": './src/platforms/web/*',
+    "weex/*": './src/platforms/weex/*',
+    "server/*": './src/server/*',
+    "sfc/*": './src/sfc/*',
+  }
+})
